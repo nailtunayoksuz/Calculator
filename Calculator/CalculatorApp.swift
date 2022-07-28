d6f8e5461e937060691295515f569663b4accc4d
Nail Tunay ÖKSÜZ <nailtunayoksuz@hotmail.com>
2022-07-28 23:36:01 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Nail Tunay ÖKSÜZ on 28.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
